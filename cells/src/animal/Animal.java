8b571e9b4d5d9fe17ff66defa09342965cf872a8
liujiankan <125016756+liujiankan@users.noreply.github.com>
2023-08-22 13:50:04 +0800

    Add files via upload

---
+++ cells/src/animal/Animal.java
@@ -0,0 +1,15 @@
+package animal;
+
+/**
+ * @author liujiankang liujiankang@junying-tech.com
+ * @version 2023/5/6 17:22
+ * @since JDK8
+ */
+public class Animal {
+    private int age;
+    private int ageLimit = 20;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ cells/src/animal/Robbit.java
@@ -0,0 +1,12 @@
+package animal;
+
+/**
+ * @author liujiankang liujiankang@junying-tech.com
+ * @version 2023/5/6 17:24
+ * @since JDK8
+ */
+public class Robbit extends Animal{
+    public static void main(String[] args) {
+
+    }
+}
